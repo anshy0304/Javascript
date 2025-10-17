ea820f2634ff74901026609648b73d17e39f5cb7
anshy0304 <anshy030405@gmail.com>
2025-10-17 12:59:11 +0530

    02_datatypes.js

---
+++ basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use Strict"; /*new version of javascript*/
+// alert(3+3) we are using nodejs not browse 
+
+ // variables
+ let name = "ansh"
+ let age = 20
+ let isLoggedIn = false
+  // number <= 2 to power 3
+  // bigint
+  // string
+  // boolean - true or false
+  // null - > standalone vlaue
+  // undefined -> not assigned value
+  // symbol -> unique
+
+  // object
+  console.log(typeof )
